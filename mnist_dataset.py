5506ca0a7bc2f8daaa6d98b5bb2909d49d64614f
sumit <sumit.nautiyal@rahisystems.com>
2021-11-27 23:09:45 +0530

    dataset output

--- mnist_dataset.py
+++ mnist_dataset.py
@@ -3,17 +3,3 @@ from matplotlib import pyplot
 
 # loading the dataset and storing into different variables
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
-
-# shape of dataset
-print('X_train: ' + str(train_X.shape))
-print('Y_train: ' + str(train_y.shape))
-print('X_test:  ' + str(test_X.shape))
-print('Y_test:  ' + str(test_y.shape))
-
-# plotting them using matplotlib
-from matplotlib import pyplot
-
-for i in range(9):
-    pyplot.subplot(330 + 1 + i)
-    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
-pyplot.show()
